ab7290127931b1131fbe70ade3900f8f59135acf
DavidMartinez <davidf.martinez.br@gmail.com>
2022-09-06 16:32:06 -0500

    adding css and navbar for home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import NavBar from './components/NavBar';
+
 function App() {
 	return (
-		<div className="App">
-			<h1>Hello!</h1>
+		// <div className="App">
+		<div>
+			<NavBar />
 		</div>
 	);
 }
